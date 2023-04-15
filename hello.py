15cc1091345187b5e8c575677d3d7aea229b3ddd
emma1234534 <118064673+emma1234534@users.noreply.github.com>
2023-04-15 13:10:56 +0200

    Add files via upload
    my first github upload

---
+++ hello.py
@@ -0,0 +1,51 @@
+# name = "breezy"
+# age = 14
+# if(age < 18):
+#     print("your kiddo")
+# else:
+#     print("u have big 
+
+# name =input("enter name ")
+
+# age =input("enter age ")
+
+# if(name == "breezy"):
+#     print("yeah is breezy")
+
+# elif(age == 20):
+#     print("yeah my age ")
+# else:
+#     print("nahh breezy")
+
+# age =20.4
+# name = "fvfgug bjbbjbb"
+
+# def greating(name):
+#     print("hello world",name)
+
+# greating("dog")
+
+
+# def multipy(x,y,z):
+#     print(x*y*z)
+
+
+
+# multipy(4,4)
+# greating()
+# greating()
+# greating()
+# greating()
+
+
+def validKids():
+    Name = input("Enter name ")
+    Age = input("Enter age ")
+
+    if(Name == "breezy" and Age == 20):
+        print("yeah am validated")
+    else:
+        print("your thief")
+
+
+validKids()
